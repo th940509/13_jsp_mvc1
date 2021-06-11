8a2c6cebcd39bf138a7333c42dcbff47773ea239
user <user@401-10>
2021-06-11 17:46:39 +0900

    06/11

--- 13_jsp_mvc1/src/step2_00_loginEx/MemberDAO.java
+++ 13_jsp_mvc1/src/step2_00_loginEx/MemberDAO.java
@@ -88,4 +88,65 @@ public class MemberDAO {
 		
 		return isValidMember;
 	}
+	
+	// delete DAO
+	public boolean leaveMember(String id, String pwd) {
+		
+			boolean isLeaveMember = false;
+			try {
+				conn = getConnection();
+				pstmt = conn.prepareStatement("select * from member where id=? and passwd=?");
+				pstmt.setString(1, id);
+				pstmt.setString(2, pwd);
+				rs = pstmt.executeQuery();
+				
+				if(rs.next()) {
+					pstmt = conn.prepareStatement("delete from member where id=?");
+					pstmt.setString(1, id);
+					pstmt.executeUpdate();
+					isLeaveMember = true;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				if(rs != null)    try {rs.close();}    catch (SQLException e) {e.printStackTrace();}  
+				if(pstmt != null) try {pstmt.close();} catch (SQLException e) {e.printStackTrace();}
+				if(conn != null)  try {conn.close();}  catch (SQLException e) {e.printStackTrace();}
+			}
+			
+			return isLeaveMember;
+	}
+	
+	// Update DAO
+		public boolean updateMember(MemberDTO mdto) {
+			
+			boolean isUpdateMember = false;
+			
+			try {
+				
+				conn = getConnection();
+				pstmt = conn.prepareStatement("select * from member where id=? and passwd=?");
+				pstmt.setString(1, mdto.getId());
+				pstmt.setString(2, mdto.getPasswd());
+				rs = pstmt.executeQuery();
+				
+				if (rs.next()) {
+					pstmt = conn.prepareStatement("update member set name=? where id=?");
+					pstmt.setString(1, mdto.getName());
+					pstmt.setString(2, mdto.getId());
+					pstmt.executeUpdate();
+					isUpdateMember = true;
+				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				if(rs != null)    try {rs.close();}    catch (SQLException e) {e.printStackTrace();}
+				if(pstmt != null) try {pstmt.close();} catch (SQLException e) {e.printStackTrace();}
+				if(conn != null)  try {conn.close();}  catch (SQLException e) {e.printStackTrace();}
+			}
+			
+			return isUpdateMember;
+			
+		}
 }
